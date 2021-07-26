8b61fb08b2d7cd0c6fbe9aced0d659adbe371af2
taihungg <hunginception@gmail.com>
2021-07-26 11:11:05 +0700

    first commit

---
+++ db.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='localhost',
+    username='root',
+    password=''
+)
+cur = db.cursor()
+
+cur.execute("Create Database IF NOT EXISTS phimmoi")
+
+
+
+
+
+
